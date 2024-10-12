52a53d8f4c0ca5a29479e6ce62a07475bcbba050
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-10-12 11:28:52 -0300

    implementando método para listagem paginada de produtos

--- estoque-microservice/src/main/java/com/upe/estoque_microservice/service/ProdutoService.java
+++ estoque-microservice/src/main/java/com/upe/estoque_microservice/service/ProdutoService.java
@@ -1,11 +1,29 @@
 package com.upe.estoque_microservice.service;
 
+import com.upe.estoque_microservice.config.dto.ProdutoRespostaDto;
 import com.upe.estoque_microservice.repository.ProdutoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ProdutoService {
   private final ProdutoRepository produtoRepository;
+
+  public Page<ProdutoRespostaDto> listarProdutos(int pagina, int tamanho) {
+    Pageable pageable = PageRequest.of(pagina, tamanho);
+    return this.produtoRepository
+        .findAll(pageable)
+        .map(
+            produto ->
+                ProdutoRespostaDto.builder()
+                    .id(produto.getId())
+                    .nome(produto.getNome())
+                    .preco(produto.getPreco())
+                    .quantidadeDisponivel(produto.getQuantidadeDisponivel())
+                    .build());
+  }
 }
