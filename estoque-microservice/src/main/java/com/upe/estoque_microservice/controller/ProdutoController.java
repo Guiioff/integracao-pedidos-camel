b53aca05e0ad0521888ec8fc7c859ef174bf2f83
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-10-12 11:29:08 -0300

    implementando endpoint para listagem paginada de produtos

--- estoque-microservice/src/main/java/com/upe/estoque_microservice/controller/ProdutoController.java
+++ estoque-microservice/src/main/java/com/upe/estoque_microservice/controller/ProdutoController.java
@@ -1,8 +1,14 @@
 package com.upe.estoque_microservice.controller;
 
+import com.upe.estoque_microservice.config.dto.ProdutoRespostaDto;
 import com.upe.estoque_microservice.service.ProdutoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/produtos")
 public class ProdutoController {
   private final ProdutoService produtoService;
+
+  @GetMapping
+  public ResponseEntity<Page<ProdutoRespostaDto>> listarProdutos(
+      @RequestParam(defaultValue = "0") int pagina,
+      @RequestParam(defaultValue = "10") int tamanho) {
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(this.produtoService.listarProdutos(pagina, tamanho));
+  }
 }
