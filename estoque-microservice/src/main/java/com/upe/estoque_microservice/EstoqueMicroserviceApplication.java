7eb76f0af0f4b85e0079613fba68205e069d6fbe
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-10-12 10:53:00 -0300

    configurando o arquivo .gitignore

--- estoque-microservice/src/main/java/com/upe/estoque_microservice/EstoqueMicroserviceApplication.java
+++ estoque-microservice/src/main/java/com/upe/estoque_microservice/EstoqueMicroserviceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EstoqueMicroserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EstoqueMicroserviceApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(EstoqueMicroserviceApplication.class, args);
+  }
 }
