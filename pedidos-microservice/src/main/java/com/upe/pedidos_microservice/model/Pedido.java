9532130de3fcf95c326f71f46bd2fd75554ddccb
Guiioff <guilhermerivaldy288@hotmail.com>
2024-10-16 19:53:04 -0300

    criando classe Pedido

---
+++ pedidos-microservice/src/main/java/com/upe/pedidos_microservice/model/Pedido.java
@@ -0,0 +1,28 @@
+package com.upe.pedidos_microservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tb_pedido")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String produtoId;
+
+    @Column(nullable = false)
+    private int quantidade;
+
+    @Column(nullable = false)
+    private String nomeCliente;
+
+}
