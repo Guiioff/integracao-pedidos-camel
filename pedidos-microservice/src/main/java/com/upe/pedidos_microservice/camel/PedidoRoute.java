571f4e701d42be7e6ad7cc0b04703f752323897a
Guiioff <guilhermerivaldy288@hotmail.com>
2024-10-16 21:13:23 -0300

    criando rota do camel do Pedido

---
+++ pedidos-microservice/src/main/java/com/upe/pedidos_microservice/camel/PedidoRoute.java
@@ -0,0 +1,13 @@
+package com.upe.pedidos_microservice.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PedidoRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
