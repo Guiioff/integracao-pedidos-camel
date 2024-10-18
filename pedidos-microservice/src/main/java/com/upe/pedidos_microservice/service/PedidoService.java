c39e3ee3b3e20d7969b80bfe34315a5508713a74
Guiioff <guilhermerivaldy288@hotmail.com>
2024-10-17 23:11:02 -0300

    adicionando processo de envio do Pedido para a fila

--- pedidos-microservice/src/main/java/com/upe/pedidos_microservice/service/PedidoService.java
+++ pedidos-microservice/src/main/java/com/upe/pedidos_microservice/service/PedidoService.java
@@ -4,15 +4,18 @@ import com.upe.pedidos_microservice.controller.dtos.PedidoRequestDto;
 import com.upe.pedidos_microservice.model.Pedido;
 import com.upe.pedidos_microservice.repository.PedidoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PedidoService {
     private final PedidoRepository pedidoRepository;
+    private final JmsTemplate jmsTemplate;
 
     public Pedido salvarPedido(PedidoRequestDto dto){
         Pedido pedido = new Pedido(dto);
+        jmsTemplate.convertAndSend("my-pedidos-queue", pedido);
         return pedidoRepository.save(pedido);
     }
 }
