d7a0c809d09cb312d7c64214f241342eeca604c0
Guiioff <guilhermerivaldy288@hotmail.com>
2024-10-18 21:45:48 -0300

    adicionando processo de envio do pedido para a fila do activemq

--- pedidos-microservice/src/main/java/com/upe/pedidos_microservice/service/PedidoService.java
+++ pedidos-microservice/src/main/java/com/upe/pedidos_microservice/service/PedidoService.java
@@ -1,21 +1,45 @@
 package com.upe.pedidos_microservice.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.upe.pedidos_microservice.controller.dtos.PedidoRequestDto;
 import com.upe.pedidos_microservice.model.Pedido;
 import com.upe.pedidos_microservice.repository.PedidoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class PedidoService {
     private final PedidoRepository pedidoRepository;
     private final JmsTemplate jmsTemplate;
+    private final ObjectMapper objectMapper;
 
+    @Transactional
     public Pedido salvarPedido(PedidoRequestDto dto){
         Pedido pedido = new Pedido(dto);
-        jmsTemplate.convertAndSend("my-pedidos-queue", pedido);
+
+        try {
+            String pedidoJson = converterParaJson(pedido);
+            enviarMensagem(pedidoJson);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Erro ao converter Pedido para JSON", e);
+        }
+
         return pedidoRepository.save(pedido);
+    }
+
+    private String converterParaJson(Pedido pedido) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(pedido);
+    }
+
+    private void enviarMensagem(String message) {
+        try {
+            jmsTemplate.convertAndSend("pedidos-queue", message);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao enviar Pedido para a fila", e);
+        }
     }
 }
