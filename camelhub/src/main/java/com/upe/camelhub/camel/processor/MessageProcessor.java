fa4e47a185b07febf6e43f256acdb9dcaa7f5093
Guiioff <guilhermerivaldy288@hotmail.com>
2024-10-22 20:41:02 -0300

    criação das rotas e do processor do camel

---
+++ camelhub/src/main/java/com/upe/camelhub/camel/processor/MessageProcessor.java
@@ -0,0 +1,32 @@
+package com.upe.camelhub.camel.processor;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+import java.io.IOException;
+
+@Slf4j
+public class MessageProcessor implements Processor {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void process(Exchange exchange) throws IOException {
+        String jsonBody = exchange.getIn().getBody(String.class);
+        log.info("Corpo da mensagem recebida: " + jsonBody);
+
+        JsonNode jsonNode = objectMapper.readTree(jsonBody);
+
+        Long produtoId = jsonNode.path("produtoId").asLong();
+        int quantidade = jsonNode.path("quantidade").asInt();
+
+        ObjectNode resultadoJson = objectMapper.createObjectNode();
+        resultadoJson.put("id", produtoId);
+        resultadoJson.put("quantidade", quantidade);
+
+        exchange.getIn().setBody(resultadoJson.toString());
+    }
+}
