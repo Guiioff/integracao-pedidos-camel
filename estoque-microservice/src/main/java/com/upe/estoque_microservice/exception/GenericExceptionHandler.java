7fe7ea453f06c79f391b3d4b6f33f4ed5bd0af5a
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-10-22 22:22:42 -0300

    implementando handler para lidar com exceções de operação não permitida

--- estoque-microservice/src/main/java/com/upe/estoque_microservice/exception/GenericExceptionHandler.java
+++ estoque-microservice/src/main/java/com/upe/estoque_microservice/exception/GenericExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.upe.estoque_microservice.exception;
 
 import com.upe.estoque_microservice.exception.exceptions.NaoEncontradoException;
+import com.upe.estoque_microservice.exception.exceptions.OperacaoNaoPermitidaException;
 import com.upe.estoque_microservice.exception.message.ErroDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GenericExceptionHandler {
 
   private static final String NAO_ENCONTRADO_ERRO_TITULO = "Elemento não encontrado";
+  private static final String OPERACAO_NAO_PERMITIDA_ERRO_TITULO =
+      "Operação não permitida ou não autorizada";
 
   @ExceptionHandler(NaoEncontradoException.class)
   public ResponseEntity<ErroDto> handleNaoEncontradoException(NaoEncontradoException ex) {
@@ -18,5 +21,15 @@ public class GenericExceptionHandler {
         new ErroDto(HttpStatus.NOT_FOUND.value(), NAO_ENCONTRADO_ERRO_TITULO, ex.getMessage());
 
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+  }
+
+  @ExceptionHandler(OperacaoNaoPermitidaException.class)
+  public ResponseEntity<ErroDto> handleOperacaoNaoPermitidaException(
+      OperacaoNaoPermitidaException ex) {
+    ErroDto erro =
+        new ErroDto(
+            HttpStatus.CONFLICT.value(), OPERACAO_NAO_PERMITIDA_ERRO_TITULO, ex.getMessage());
+
+    return ResponseEntity.status(HttpStatus.CONFLICT).body(erro);
   }
 }
