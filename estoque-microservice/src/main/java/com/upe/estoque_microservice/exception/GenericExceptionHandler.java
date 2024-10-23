4651d5e83036b06f40739ffad10e010763831138
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-10-22 22:12:34 -0300

    criando handler genérico para as exceções do sistema

---
+++ estoque-microservice/src/main/java/com/upe/estoque_microservice/exception/GenericExceptionHandler.java
@@ -0,0 +1,6 @@
+package com.upe.estoque_microservice.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GenericExceptionHandler {}
