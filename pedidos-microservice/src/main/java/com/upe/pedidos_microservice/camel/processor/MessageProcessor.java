9ea3efe8889279b4df778e1620ae383706a34792
Guiioff <guilhermerivaldy288@hotmail.com>
2024-10-22 23:49:26 -0300

    corrigindo erro do envio da mensagem para o rabbitmq

--- pedidos-microservice/src/main/java/com/upe/pedidos_microservice/camel/processor/MessageProcessor.java
+++ pedidos-microservice/src/main/java/com/upe/pedidos_microservice/camel/processor/MessageProcessor.java
@@ -6,14 +6,12 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
-import java.io.IOException;
-
 public class MessageProcessor implements Processor {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
-    public void process(Exchange exchange) throws IOException {
+    public void process(Exchange exchange) throws Exception {
         String jsonBody = exchange.getIn().getBody(String.class);
 
         JsonNode jsonNode = objectMapper.readTree(jsonBody);
@@ -25,6 +23,8 @@ public class MessageProcessor implements Processor {
         resultadoJson.put("id", produtoId);
         resultadoJson.put("quantidade", quantidade);
 
-        exchange.getIn().setBody(resultadoJson.toString());
+        byte[] resultadoBytes = objectMapper.writeValueAsBytes(resultadoJson);
+
+        exchange.getIn().setBody(resultadoBytes);
     }
 }
