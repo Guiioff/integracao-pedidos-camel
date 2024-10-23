bd6096c9aa04ee24ab8b181fea8baefa42a32349
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-10-22 23:11:14 -0300

    implementando log no listener da fila do RabbitMQ

--- estoque-microservice/src/main/java/com/upe/estoque_microservice/listener/EstoqueListener.java
+++ estoque-microservice/src/main/java/com/upe/estoque_microservice/listener/EstoqueListener.java
@@ -4,16 +4,22 @@ import com.upe.estoque_microservice.config.RabbitMQConfig;
 import com.upe.estoque_microservice.listener.message.AlterarEstoqueMessage;
 import com.upe.estoque_microservice.service.ProdutoService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class EstoqueListener {
   private final ProdutoService produtoService;
 
   @RabbitListener(queues = RabbitMQConfig.QUEUE_NAME)
   public void listenAlterarEstoqueQueue(AlterarEstoqueMessage message) {
-    this.produtoService.alterarQuantidadeProduto(message.id(), message.quantidade());
+    try {
+      this.produtoService.alterarQuantidadeProduto(message.id(), message.quantidade());
+    } catch (Exception ex) {
+      log.error("Erro na operação: " + ex.getMessage());
+    }
   }
 }
